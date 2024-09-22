f00205071b65b223343071ba8bf2b88a0b8d5243
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-22 22:04:49 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCv44uPyTiAaqoxzrHgA24Tmt1zy1DP1WI",
+            authDomain: "todo-scxvrd.firebaseapp.com",
+            projectId: "todo-scxvrd",
+            storageBucket: "todo-scxvrd.appspot.com",
+            messagingSenderId: "57828322430",
+            appId: "1:57828322430:web:007af8a09ce9686c083c2f"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
---
+++ lib/login/login_model.dart
@@ -0,0 +1,138 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'login_widget.dart' show LoginWidget;
+import 'package:flutter/material.dart';
+
+class LoginModel extends FlutterFlowModel<LoginWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final formKey2 = GlobalKey<FormState>();
+  final formKey1 = GlobalKey<FormState>();
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+
+  // State field(s) for signup-email widget.
+  FocusNode? signupEmailFocusNode;
+  TextEditingController? signupEmailTextController;
+  String? Function(BuildContext, String?)? signupEmailTextControllerValidator;
+  String? _signupEmailTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Field is required';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'Has to be a valid email address.';
+    }
+    return null;
+  }
+
+  // State field(s) for Singup-pass widget.
+  FocusNode? singupPassFocusNode;
+  TextEditingController? singupPassTextController;
+  late bool singupPassVisibility;
+  String? Function(BuildContext, String?)? singupPassTextControllerValidator;
+  String? _singupPassTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Field is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for signup-confirmpass widget.
+  FocusNode? signupConfirmpassFocusNode;
+  TextEditingController? signupConfirmpassTextController;
+  late bool signupConfirmpassVisibility;
+  String? Function(BuildContext, String?)?
+      signupConfirmpassTextControllerValidator;
+  String? _signupConfirmpassTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Field is Required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for Login-Email widget.
+  FocusNode? loginEmailFocusNode;
+  TextEditingController? loginEmailTextController;
+  String? Function(BuildContext, String?)? loginEmailTextControllerValidator;
+  String? _loginEmailTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Field is Required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'Has to be a valid email address.';
+    }
+    return null;
+  }
+
+  // State field(s) for Login-Oassword widget.
+  FocusNode? loginOasswordFocusNode;
+  TextEditingController? loginOasswordTextController;
+  late bool loginOasswordVisibility;
+  String? Function(BuildContext, String?)? loginOasswordTextControllerValidator;
+  String? _loginOasswordTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Field is Required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  @override
+  void initState(BuildContext context) {
+    signupEmailTextControllerValidator = _signupEmailTextControllerValidator;
+    singupPassVisibility = false;
+    singupPassTextControllerValidator = _singupPassTextControllerValidator;
+    signupConfirmpassVisibility = false;
+    signupConfirmpassTextControllerValidator =
+        _signupConfirmpassTextControllerValidator;
+    loginEmailTextControllerValidator = _loginEmailTextControllerValidator;
+    loginOasswordVisibility = false;
+    loginOasswordTextControllerValidator =
+        _loginOasswordTextControllerValidator;
+  }
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+    signupEmailFocusNode?.dispose();
+    signupEmailTextController?.dispose();
+
+    singupPassFocusNode?.dispose();
+    singupPassTextController?.dispose();
+
+    signupConfirmpassFocusNode?.dispose();
+    signupConfirmpassTextController?.dispose();
+
+    loginEmailFocusNode?.dispose();
+    loginEmailTextController?.dispose();
+
+    loginOasswordFocusNode?.dispose();
+    loginOasswordTextController?.dispose();
+  }
+}
